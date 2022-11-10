5f21359ea666bc435f650c9cae4da92b2bb76b5e
Michael Toy <miket2997@yahoooo.com>
2022-11-09 19:08:10 -0500

    adding loop olympics and practice

---
+++ exercises/nested-loop-practice/nested.js
@@ -0,0 +1,15 @@
+var arrayOfArrays = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+for(var i = 0; i < arrayOfArrays.length; i++){
+    for(var j = 0; j < arrayOfArrays[i].length; j++){
+        //console.log(arrayOfArrays[i][j]);
+    }
+}
+
+//console.log(arrayOfArrays[1][2]);
+//console.log(arrayOfArrays[0][0]);
+console.log(arrayOfArrays[2][2]);
---
+++ loop-olympics/lo.js
@@ -0,0 +1,156 @@
+
+//Preliminaries
+// 1.
+//for(var i = 0; i <= 9; i++){
+    //console.log(i);
+//}
+
+// 2.
+//for(var i = 9; i >= 0; i--){
+    //console.log(i)
+//}
+
+// 3.
+//const fruit = ["banana", "orange", "apple", "kiwi",];
+//for(var i = 0; i < fruit.length; i ++){
+    //console.log(fruit[i]);
+//}
+
+
+
+
+//Bronze
+
+
+// 1.
+//var numberArray = [];
+//for(var i = 0; i < 10; i++){
+    //numberArray.push(i);
+//}
+//console.log(numberArray);
+
+
+
+// 2.
+//for(var i = 0; i <= 100; i++){
+    //if(i % 2 === 0){
+        //console.log(i)
+    //}
+//}
+
+
+// 3.
+
+//const fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"];
+//var fruitArray = [];
+
+
+//for(var i = 0; i <fruit.length; i++){
+    //if(i % 2 === 0){
+        //fruitArray.push(fruit[i])
+    //}
+//}
+//console.log(fruitArray)
+
+
+
+
+
+
+
+
+//Silver
+
+
+//const peopleArray = [
+    //{
+      //name: "Harrison Ford",
+      //occupation: "Actor"
+    //},
+   // {
+      //name: "Justin Bieber",
+     // occupation: "Singer"
+   // },
+   // {
+    //  name: "Vladimir Putin",
+     // occupation: "Politician"
+  //  },
+  //  {
+    //  name: "Oprah",
+    //  occupation: "Entertainer"
+   // }
+ // ]
+  
+  // ["Harrison Ford", "Vladimir Putin"] // names
+  // ["Singer", "Entertainer"] // occupations
+
+//1.
+//for(var i = 0; i < peopleArray.length; i++){
+    //console.log(peopleArray[i].name)
+//}
+
+
+
+// 2.
+//var namesArray = [];
+//var occupationArray = [];
+
+//for(var i = 0; i <peopleArray.length; i++){
+    //namesArray.push(peopleArray[i].name)
+    //occupationArray.push(peopleArray[i].occupation)
+//}
+//console.log(namesArray, occupationArray);
+
+
+
+// 3.
+
+
+//var namesArray = [];
+//var occupationsArray = [];
+
+//for(var i = 0; i <peopleArray.length; i++){
+    //if(i % 2 === 0){
+        //namesArray.push(peopleArray[i].name)
+    //} if(i % 2 === 1){
+        //occupationsArray.push(peopleArray[i].occupation)
+    //}
+//}
+    
+//console.log(namesArray, occupationsArray);
+
+
+
+
+
+
+
+//Gold
+
+
+// 1.
+
+//[[0, 0, 0],
+//[0, 0, 0],
+//[0, 0, 0]]
+
+
+//var num = 3;
+//var arry = [];
+
+//for(var i = 0; i < num; i++){
+    //arry[i] = [];
+    //for(var j = 0; j < num; j++){
+        //arry[i][j]= 0;
+    //}
+//}
+//console.log(arry);
+
+
+// 2.
+
+//[[0, 0, 0],
+//[1, 1, 1],
+//[2, 2, 2]]
+
+
