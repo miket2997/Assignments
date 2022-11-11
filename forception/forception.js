60ca7ad0364bc5466022575f8091db2f9a87d8ca
Michael Toy <miket2997@yahoooo.com>
2022-11-11 14:10:31 -0500

    adding forception assignment

--- forception/forception.js
+++ forception/forception.js
@@ -1,12 +1,17 @@
-var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"];
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
+let people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"];
+let alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-var alphabetSplit = alphabet.split("");
+let alphabetSplit = alphabet.split("");
 //console.log(alphabetSplit);
 
-
-for(var i = 0; i < people.length; i++){
-    for(var j = 0; j < alphabetSplit[i].length; j++)
-   var concated = people.concat(alphabetSplit);
-    console.log(concated);
+function forception(people, alphabetSplit){
+    let nArry = [];
+    for(let i = 0; i < people.length; i++){
+        nArry.push(people[i] + ":");
+        for(let j = 0; j < alphabetSplit.length; j++){
+            nArry.push(alphabetSplit[j].toUpperCase())
+        }
+    } return nArry
 }
+
+console.log(forception(people, alphabetSplit));
