e2568dfa1c2afe335eca69814b72e4add3745e90
Michael Toy <miket2997@yahoooo.com>
2022-12-14 19:04:54 -0500

    axios practice

---
+++ exercises/axios-practice/app.js
@@ -0,0 +1,57 @@
+
+//get all
+
+
+
+
+
+
+//axios.get("https://api.vschool.io/michaeltoy/todo")
+    //.then(response => {
+       // for(let i = 0; i < response.data.length; i++){
+         //   const h1 = document.createElement("h1");
+          //  h1.textContent = response.data[i].title;
+          //  document.body.appendChild(h1)
+       // }
+  //  })
+   // .catch(error => console.log(error))
+
+//get one
+//axios.get("https://api.vschool.io/michaeltoy/todo/639904ef18d1a05bceae8e04")
+ //   .then(response => console.log(response.data))
+  //  .catch(error => console.log(error))
+
+
+
+
+//const todoForm = document.todoform;
+
+//todoForm.addEventListener("submit", function(event){
+ //   event.preventDefault()
+
+    //const newTodo = {
+      //  title: todoForm.title.value,
+      //  description: todoForm.description.value,
+       // imgUrl: todoForm.imgUrl.value
+//}
+
+    
+
+   // axios.post("https://api.vschool.io/michaeltoy/todo", newTodo)
+   // .then(response => console.log(response.data))
+    //.catch(error => console.log(error))
+
+//})
+
+//const updates = {
+    //title: "Changed title",
+   /// description: "Changed description"
+//}
+
+//axios.put("https://api.vschool.io/michaeltoy/todo/639a569818d1a05bceae8e11", updates)
+   // .then(response => console.log(response.data))
+   // .catch(error => console.log(error))
+
+//axios.get("https://api.vschool.io/michaeltoy/todo")
+   // .then(response => console.log(response.data))
+   // .catch(error => console.log(error))
