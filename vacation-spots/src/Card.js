65c70676d5edff0fcc28fa07ad5d48ba6e7b72e8
Michael Toy <miket2997@yahoooo.com>
2023-01-10 19:48:15 -0500

    vacation spots updated

--- vacation-spots/src/Card.js
+++ vacation-spots/src/Card.js
@@ -1,8 +1,9 @@
 export default function Card(props){
     return(
-        <div className="card--container">
+        <div className="card--container" style={{backgroundColor: (props.timeToGo === "Fall") ? "lightgray" : 
+        (props.timeToGo === "Summer") ? "orange" : (props.timeToGo === "Spring") ? "red" : "lightblue" }}>
             <h1>Location: {props.place}</h1>
-            <h1>Price: ${props.price}</h1>
+            <h1>Price: {(props.price < 500) ? "$" : (props.price < 1000 && props.price > 500) ? "$$" : "$$$"}{props.price}</h1>
             <h1>Best Time to Go: {props.timeToGo}</h1>
         </div>
     )
