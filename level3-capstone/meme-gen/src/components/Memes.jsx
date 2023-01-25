e0be7bfe37d4f16e25245e11ed8638c83b8db289
Michael Toy <miket2997@yahoooo.com>
2023-01-25 04:10:06 -0500

    updated css

--- level3-capstone/meme-gen/src/components/Memes.jsx
+++ level3-capstone/meme-gen/src/components/Memes.jsx
@@ -6,12 +6,13 @@ export default function Memes(props){
     return (
         <div className="meme--container">
             <img src= {randomImage} className="image--list" />
-            <h1 className="top--text--list">{topText}</h1>
-            <h1 className="bottom--text--list">{bottomText}</h1>
+            <h1 className="list--text top--text">{topText}</h1>
+            <h1 className="list--text bottom--text">{bottomText}</h1>
             <div className="buttons">
                 <button className="edit--button" onClick={() => props.edit(id)}>Edit</button>
                 <button className="delete--button" onClick={() => props.delete(id)}>Delete</button>
             </div>
         </div>
+
     )
 }
