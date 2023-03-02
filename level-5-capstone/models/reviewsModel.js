3db211f01294fa5f1f17cfc7a6981222f1bbc25e
Michael Toy <miket2997@yahoooo.com>
2023-03-02 00:18:28 -0500

    level 5 capstone

---
+++ level-5-capstone/models/reviewsModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    rating: {
+        type: Number,
+        required: true
+    },
+    text : {
+        type: String,
+        required: true
+    },
+    trail: {
+        type: Schema.Types.ObjectId,
+        ref: "Trails",
+        required: true
+    },
+});
+
+
+module.exports = mongoose.model("Reviews", reviewSchema)
